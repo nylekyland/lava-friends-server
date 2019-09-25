02eee00f4efaaf2330ee301f75053211b9803ba6
Kyle Nyland <knyland@ameritech.net>
2019-09-25 14:30:16 -0400

    testing sending messages

--- index.js
+++ index.js
@@ -3,18 +3,26 @@ var http = require("http")
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
+var players = {};
+var colors = ['red', 'yellow', 'green', 'blue'];
 
 app.use(express.static(__dirname + "/"))
 
 var server = http.createServer(app)
-server.listen(port)
-
-console.log("http server listening on %d", port)
+server.listen(port, function(){
+	console.log("http server listening on %d", port)
+});
 
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
+/*
+*  A new player has connected
+*/
 wss.on("connection", function(ws) {
+  
+  ws.send('hello from the server!');
+  
   var id = setInterval(function() {
     ws.send(JSON.stringify(new Date()), function() {  })
   }, 1000)
