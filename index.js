7c9c0c5b047d844e76c7f1bd8562b0b760610a2d
Kyle Nyland <knyland@ameritech.net>
2019-09-30 12:25:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -37,11 +37,29 @@ wss.on("connection", function(ws) {
   }, 1000)
   
   ws.on('message', function incoming(data) {
-	ws.send('message function entered!: ' + data);
+	handlePlayerMovement(data);
   });
 
   ws.on("close", function() {
     console.log("websocket connection close")
     clearInterval(id)
   })
-})
+});
+
+var handlePlayerMovement = function(data){
+	var leftPressed = false,
+	rightPressed = false;
+	//Position 1: Left is pressed
+	leftPressed = !!(data & 1);
+	//Position 2: Right is pressed
+	rightPressed = !!(data & 2);
+	
+	//Player logic
+	if (leftPressed)
+		players[ws.id] -= 0.5;
+	if (rightPressed)
+		players[ws.id] += 0.5;
+	
+	//Send the updated data back to the client
+	ws.send(JSON.stringify(player[ws.id]));
+}
