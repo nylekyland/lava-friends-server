21decdc30eaf014fbedd46e25b9198bc43dfe4cc
Kyle Nyland <knyland@ameritech.net>
2019-10-15 15:42:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -120,8 +120,12 @@ wss.on("connection", function(ws) {
 });
 
 function rectangleOverlap(rect1, rect2){
-	return !(rect1.x > rect2.x + rect2.width ||
-   rect1.x + rect1.width < rect2.x ||
-   rect1.y > rect2.y + rect2.height ||
-   rect1.y + rect1.height < rect2.y);
+	console.log(rect1.x < rect2.x + rect2.width &&
+   rect1.x + rect1.width > rect2.x &&
+   rect1.y < rect2.y + rect2.height &&
+   rect1.y + rect1.height > rect2.y);
+	return (rect1.x < rect2.x + rect2.width &&
+   rect1.x + rect1.width > rect2.x &&
+   rect1.y < rect2.y + rect2.height &&
+   rect1.y + rect1.height > rect2.y);
 }
