1eb35dcc74e141e3d79b00a95a302b5a76544a65
Kyle Nyland <knyland@ameritech.net>
2019-10-22 12:16:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ wss.on("connection", function(ws) {
 			x: players[ws.id].x,
 			y: players[ws.id].y,
 			width: players[ws.id].width,
-			height: players[ws.id].height
+			height: players[ws.id].height + yVelocity + gravity
 		}
 		if (rectangleOverlap(blocks[block], newObj)){
 			objectBeneath = blocks[block];
