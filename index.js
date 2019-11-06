793b2b2b97a2a9bd11421bab51b3c3b535815a8b
Kyle Nyland <knyland@ameritech.net>
2019-11-06 13:45:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ wss.on("connection", function(ws) {
 	timerRef = setInterval(countdown, 1000);
   }
   
-  updateRef = setInterval(function(){updatePositions(players[ws.id])}, 0.3);
+  updateRef = setInterval(function(){updatePositions(players[ws.id])}, 30);
   
   ws.on('message', function incoming(json) {
 	var data = JSON.parse(json);
