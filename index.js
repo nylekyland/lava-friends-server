5b9ce32901fb31d20312cf2ff345f01a7a399286
Kyle Nyland <knyland@ameritech.net>
2020-01-15 17:02:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -831,9 +831,9 @@ function checkHitbox(currentPlayer, hitbox){
 				players[obj].stunned = true;
 				players[obj].yVelocity = -10;
 				if (currentPlayer.punchLeftRight == "right")
-					players[obj].xVelocity = 6;
+					players[obj].xVelocity = 24;
 				else
-					players[obj].xVelocity = -6;
+					players[obj].xVelocity = -24;
 			}
 		}
 	}
