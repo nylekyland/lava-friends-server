2b4c3a586d43a5517bfcbe3dbec6604e106708c8
Kyle Nyland <knyland@ameritech.net>
2020-03-31 15:09:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -381,11 +381,11 @@ function getHighestBlockY(game) {
 }
 
 function getHighestPlayer(game){
-	var highest = -1000;
+	var highest = 500;
 	for (var obj in players){
 		if (players[obj].gameId == game.id){
-			if (players[obj].y - 500 <= highest)
-				highest = players[obj].y - 500;
+			if (players[obj].y <= highest)
+				highest = players[obj].y;
 		}
 	}
 	return highest;
@@ -871,10 +871,10 @@ function updateGame(game) {
 	}
 	var highestY = getHighestPlayer(game);
 	if (game.blocks[1].y <= highestY){
-		game.blocks[1].y = highestY;
-		game.blocks[1].height = 600 - highestY;
-		game.blocks[2].y = highestY;
-		game.blocks[2].height = 600 - highestY;
+		game.blocks[1].y = highestY - 1000;
+		game.blocks[1].height = 1600 - highestY;
+		game.blocks[2].y = highestY - 1000;
+		game.blocks[2].height = 1600 - highestY;
 	}
 }
 
